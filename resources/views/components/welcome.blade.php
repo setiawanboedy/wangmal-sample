7ab0698eb227e5863755d8116766e662cee8f09c
setiawanboedy <gonisetiawan0@gmail.com>
2023-12-24 13:32:36 +0800

    init

---
+++ resources/views/components/welcome.blade.php
@@ -0,0 +1,16 @@
+<div class="p-6 bg-white border-b border-gray-200 lg:p-8">
+    <x-application-logo class="block w-auto h-12" />
+
+    <h1 class="mt-8 text-2xl font-medium text-gray-900">
+        Welcome to your Jetstream application!
+    </h1>
+
+    <p class="mt-6 leading-relaxed text-gray-500">
+        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
+        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
+        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
+        ecosystem to be a breath of fresh air. We hope you love it.
+    </p>
+</div>
+
+
